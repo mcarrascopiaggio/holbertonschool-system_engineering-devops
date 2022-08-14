111bb12ab29ff97c3545239a0ee7c9fc0af77e9c
root <mcarrascopiaggio@gmail.com>
2022-08-13 17:52:40 -0700

    first version

---
+++ 100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Setting up client ssh
+file_line { 'Turn off passwd auth':
+  path     => '/etc/ssh/ssh_config',
+  line     => ' PasswordAuthentication no',
+  replace  => true,
+}
+file_line { 'Declare identity file':
+  path     => '/etc/ssh/ssh_config',
+  line     => ' IdentityFile ~/.ssh/school',
+  replace  => true,
+}
